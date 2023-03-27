5053a7af4d6b4ab7839ae37b22ef54ee24c7cc9f
Ahmad1234Mehmood <ahmad.mehmood1236@gmail.com>
2023-03-27 20:39:39 +0500

    sign up3 page update

--- src/Pages/Components/Page/Signup/index.js
+++ src/Pages/Components/Page/Signup/index.js
@@ -8,6 +8,7 @@ function Singup() {
  
     <div className={style.main}>
         <div className={style.Rec2}>
+        <div className={style.Rec1}> 
           
         <div className={style.Rec3}>
 
@@ -38,7 +39,7 @@ function Singup() {
                   <div className={style.Bbio}>
                   <label htmlFor="bi">Bio</label>
         
-        <input className={style.Bio}type="text" placeholder="Not more than 100 Words" />
+        <textarea className={style.Bio}type="text" placeholder="Not more than 100 Words" />
 
                   <div className={style.bb}> 
                  
@@ -60,9 +61,18 @@ function Singup() {
                   </div>
                 
                   </div>
-
-
                   <div className={style.Gender}>
+<label className={style.gender_h} htmlFor="id">Gender:
+        
+        </label>
+  
+<input type="radio" name="male" value="" id="gmale"/>
+<label for="gender-male">Male</label>
+
+<input type="radio" name="female" value="" id="gfemale"/>
+<label for="gender-female">Female</label>
+</div>
+                  {/* <div className={style.Gender}>
                     
                   <label className={style.gender_h} htmlFor="id">Gender:
         
@@ -73,7 +83,7 @@ function Singup() {
     
           <input className={style.fgender} type="radio" name="gender" value="male"male/>
              <label htmlFor="genderf">Female</label>
-                  </div>
+                  </div> */}
 
             
 
@@ -93,119 +103,11 @@ function Singup() {
 
             </div>
         </div>
+
         </div>
-
-
-
+        </div>
     </div>
     );
-
-
-
-
-
-
-
-
-
-  // return <div>Singup3</div>;
-
-//   return (
- 
-//     <div className="main">
-//         <div className="Rec2">
-            
-//         <div className="Rec3">
-
-//             <div className="Rec4">
-//                 <div className="Rec5">
-
-//                    <div className="Rec6">
-                  
-//                     <div className="top_bar"> 
-//                  <div className="logo">
-//                  <img className="icon" alt="" src="../log.svg" />
-
-//                  </div>
-                 
-                 
-              
-//                  <div className="logout">
-//                  <button className="logout_btn">Logout</button>
-//                  </div>
-                 
-// </div>
-
-//                  <div className="about_you">
-//                     <button className="about"> About you </button>
-//                  </div>
-//                  <div className="innerrec">
-//                    <div className="Rec7">
-
-//                   <div className="Bbio">
-//                   <label htmlFor="bi">Bio</label>
-        
-//         <input className="Bio"type="text" placeholder="Not more than 100 Words" />
-
-//                   <div className="bb"> 
-                 
-//                   <div className="Country">
-//                   <label htmlFor="cnt">Country</label>
-                 
-// <select className="country" id="country">
-// <option value="volvo">Country</option>
-//   <option value="volvo">Pakistan</option>
-//   <option value="saab">America</option>
-//   <option value="mercedes">Mexico</option>
-//   <option value="audi">France</option>
-// </select>
-//                   </div>
-
-//                   <div className="Age">
-//                   <label htmlFor="aage">Age</label>
-//                   <input className="age" type="number" placeholder="Age" />
-//                   </div>
-                
-//                   </div>
-
-
-//                   <div className="Gender">
-                    
-//                   <label className="gender_h" htmlFor="id">Gender:
-        
-//         </label>
-   
-//           <input className="mgender" type="radio" name="gender" value="male"male/>
-//              <label htmlFor="genderm" >Male</label>
-    
-//           <input className="fgender" type="radio" name="gender" value="male"male/>
-//              <label htmlFor="genderf">Female</label>
-//                   </div>
-
-            
-
-
-//                   <div className="Next">
-//                   <button className="next_btn">Next</button>
-//                   </div>
-//                   </div>
-
-                 
-//                    </div>
-                  
-//                    </div>
-//                    </div>
-
-//                 </div>
-
-//             </div>
-//         </div>
-//         </div>
-
-
-
-//     </div>
-//     );
 }
 
 export default Singup;
