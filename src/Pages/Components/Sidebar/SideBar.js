025d5bd07b8477ab7832f46e78938be9663a8906
Ahmad1234Mehmood <ahmad.mehmood1236@gmail.com>
2023-04-03 15:45:35 +0500

    video calling page update

--- src/Pages/Components/Sidebar/SideBar.js
+++ src/Pages/Components/Sidebar/SideBar.js
@@ -1,8 +1,47 @@
 import React from "react";
 import style from "./SideBar.module.css";
+import illustration from "../../../assets/icons/vid.svg";
+import illustration1 from "../../../assets/icons/msg.svg";
+import illustration2 from "../../../assets/icons/acnt.svg";
+import icn from "../../../assets/icons/icn.svg";
+import prof from "../../../assets/icons/prof.svg";
 
 const SideBar = () => {
-  return <div>Hi ther im SideBar</div>;
+  return (
+    <div class={style.sidebar}>
+      <div class={style.sidebar_btn}>
+        <img className={style.background_icon} alt="bg_img" src={icn} />
+      </div>
+
+      <div class={style.sidebar_middle}>
+        <button class={style.sidebar_btn1}>
+          <img
+            className={style.background_icon}
+            alt="bg_img"
+            src={illustration}
+          />
+        </button>
+        <button class={style.sidebar_btn2}>
+          <img
+            className={style.background_icon}
+            alt="bg_img"
+            src={illustration1}
+          />
+        </button>
+        <button class={style.sidebar_btn3}>
+          <img
+            className={style.background_icon}
+            alt="bg_img"
+            src={illustration2}
+          />
+        </button>
+      </div>
+
+      <button class={style.sidebar_btn4}>
+        <img className={style.background_icon} alt="bg_img" src={prof} />
+      </button>
+    </div>
+  );
 };
 
 export default SideBar;
