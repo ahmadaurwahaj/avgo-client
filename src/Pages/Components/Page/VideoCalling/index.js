556236dd264feb2a445d00b6a0da1eedbdbc6a10
Ahmad1234Mehmood <ahmad.mehmood1236@gmail.com>
2023-04-06 01:54:18 +0500

    video calling page update

--- src/Pages/Components/Page/VideoCalling/index.js
+++ src/Pages/Components/Page/VideoCalling/index.js
@@ -5,74 +5,59 @@ import add1 from "../../../../assets/icons/add.svg";
 import nxt from "../../../../assets/icons/nxt.svg";
 import info from "../../../../assets/icons/info.svg";
 import SideBar from "../../Sidebar/SideBar";
-import UserChat from "../../UserChat/index"
+import UserChat from "../../UserChat/index";
 function VideoCalling() {
   return (
     <div className={style.main}>
+
       <div className={style.innermain}>
-        <div className={style.left_container}>
-          <div>
-            {/* <p>VideoCalling</p> */}
-            <SideBar />
-          </div>
-        </div>
-        <div className={style.mid_container}>
-
-          <div className={style.top}>
-
-
+        <div className={style.inner}>
+          <div className={style.left_container}>
             <div>
-              <button className={style.navbar_buttons_left}>
-                <img
-                  className={style.background_icon}
-                  alt="bg_img"
-                  src={nxt}
-                />
-
-              </button>
-
-            </div>
-            <div className={style.name}>Peter</div>
-            <div className={style.navbar_buttons_right}>
-              <button className={style.add_btn}>
-                <img
-                  className={style.background_icon}
-                  alt="bg_img"
-                  src={add1}
-                />
-
-              </button>
+              {/* <p>VideoCalling</p> */}
+              <SideBar />
             </div>
           </div>
+          <div className={style.mid_container}>
+            <div className={style.top}>
+              <div>
+                <button className={style.navbar_buttons_left}>
+                  <img className={style.background_icon} alt="bg_img" src={nxt} />
+                </button>
+              </div>
+              <div className={style.name}>Peter</div>
+              <div className={style.navbar_buttons_right}>
+                <button className={style.add_btn}>
+                  <img
+                    className={style.background_icon}
+                    alt="bg_img"
+                    src={add1}
+                  />
+                </button>
+              </div>
+            </div>
 
-
-          <div className={style.info}>
-            <img
-              className={style.background_icon}
-              alt="bg_img"
-              src={info}
-            />
-            {/* <p>Quota of the day:</p> */}
-            <p className={style.info_text}>
-              <span style={{ fontSize: '8px' }}>Quota of the day: </span> <br />
-              <span style={{ fontSize: '10px' }}>Thank you everyone for joining the design critique meeting. I want everyone's opinion so please start</span>
-            </p>
+            <div className={style.info}>
+              <img className={style.background_infoicon} alt="bg_img" src={info} />
+              {/* <p>Quota of the day:</p> */}
+              <p className={style.info_text}>
+                <span style={{ fontSize: "8px" }}>Quota of the day: </span> <br />
+                <span style={{ fontSize: "10px" }}>
+                  Thank you everyone for joining the design critique meeting. I
+                  want everyone's opinion so please start
+                </span>
+              </p>
+            </div>
           </div>
 
-        </div>
-
-        <div className={style.right_container}>
-          <div>
-            {/* <p>VideoCalling</p> */}
-            <UserChat />
+          <div className={style.right_container}>
+            <div>
+              {/* <p>VideoCalling</p> */}
+              <UserChat />
+            </div>
           </div>
         </div>
-
       </div>
-
-
-
-
     </div>
   );
 }
--- src/Pages/Components/UserChat/index.js
+++ src/Pages/Components/UserChat/index.js
@@ -11,25 +11,21 @@ const UserChat = () => {
       <div className={style.col_1}>
         {/* <div className={style.msg_btn}> */}
         <div className={style.msg_h}>
-          <button className={style.msg_btn}>
-            Message (4)
-          </button>
+          <button className={style.msg_btn}>Message (4)</button>
         </div>
 
         <div className={style.msg_row}>
           <div className={style.msg_text}>
             {/* <div className={style.u_icon}> */}
 
-            <img
-              className={style.background_icon}
-              alt="bg_img"
-              src={uicon}
-            />
+            <img className={style.background_icon} alt="bg_img" src={uicon} />
             <div>
               <p className={style.para_text}>
                 <p className={style.uname}>Peter</p>
-                <p className={style.pra}>  Hello Guys! Whats your opinion?
-                  <span>&nbsp;&nbsp;&nbsp;&nbsp;12:02 pm</span>
+                <p className={style.pra}>
+                  {" "}
+                  Hello Guys! Whats your opinion?
+                  <span style={{ fontSize: "6px" }}>&nbsp;&nbsp;&nbsp;&nbsp;12:02 pm</span>
                 </p>
               </p>
             </div>
@@ -40,16 +36,15 @@ const UserChat = () => {
           <div className={style.msg_text}>
             {/* <div className={style.u_icon}> */}
 
-            <img
-              className={style.background_icon}
-              alt="bg_img"
-              src={uicon}
-            />
+            <img className={style.background_icon} alt="bg_img" src={uicon} />
             <div>
               <p className={style.para_text}>
                 <p className={style.uname}>Peter</p>
-                <p className={style.pra}>  Images are better.
-                  <span>&nbsp;&nbsp;&nbsp;&nbsp;12:03 pm</span></p>
+                <p className={style.pra}>
+                  {" "}
+                  Images are better.
+                  <span style={{ fontSize: "6px" }}>&nbsp;&nbsp;&nbsp;&nbsp;12:03 pm</span>
+                </p>
               </p>
             </div>
           </div>
@@ -69,7 +64,11 @@ const UserChat = () => {
               <p className={style.para_text}>
                 {/* <p className={style.uname}>Peter</p> */}
                 <p className={style.yourname}>You</p>
-                <p className={style.your_text}> Yes, it will decrease the loading <span>&nbsp;&nbsp;&nbsp;&nbsp;12:00 pm</span></p>
+                <p className={style.your_text}>
+                  {" "}
+                  Yes, it will decrease the loading{" "}
+                  <span style={{ fontSize: "6px" }}>&nbsp;&nbsp;&nbsp;&nbsp;12:00 pm</span>
+                </p>
               </p>
             </div>
           </div>
@@ -78,64 +77,41 @@ const UserChat = () => {
           <div className={style.msg_text}>
             {/* <div className={style.u_icon}> */}
 
-            <img
-              className={style.background_icon}
-              alt="bg_img"
-              src={uicon}
-            />
+            <img className={style.background_icon} alt="bg_img" src={uicon} />
             <div>
               <p className={style.para_text}>
                 <p className={style.uname}>Peter</p>
-                <p className={style.pra}> Anyone is up for illustration. I think
-                  there are less relatable images
-                  according to our brand
-                  <span className={style.tiime}>12:05 pm</span>
+                <p className={style.pra}>
+                  {" "}
+                  Anyone is up for illustration. I think there are less
+                  relatable images according to our brand
+                  <span style={{ fontSize: "6px" }} className={style.tiime}>12:05 pm</span>
                 </p>
               </p>
             </div>
           </div>
         </div>
 
-
         <div className={style.typing}>
-          <img
-            className={style.background_icon}
-            alt="bg_img"
-            src={ticon}
-          />
+          <img className={style.background_icon} alt="bg_img" src={ticon} />
           <p className={style.p_typing}>peter is typing</p>
         </div>
         <div className={style.message_bar}>
           <button className={style.share_btn}>
-            <img
-              className={style.background_icon}
-              alt="bg_img"
-              src={sicon}
-            />
+            <img className={style.background_i} alt="bg_img" src={sicon} />
           </button>
           <input type="text" placeholder="Write message here" />
           <button className={style.send_btn}>
             <img
-              className={style.background_icon}
+              className={style.background_ic}
               alt="bg_img"
               src={sendicon}
             />
           </button>
         </div>
       </div>
-
     </div>
-
-
-
-
-
-
-
-
-  )
-
-
+  );
 };
 
 export default UserChat;
