8d19f007bee74e9ca4503b8b537a6d2386fb199c
Ahmad1234Mehmood <ahmad.mehmood1236@gmail.com>
2023-04-08 17:07:49 +0500

    Video Calling Page Update

--- src/Pages/Components/UserChat/index.js
+++ src/Pages/Components/UserChat/index.js
@@ -13,108 +13,123 @@ const UserChat = () => {
           <button className={style.msg_btn}>Message (4)</button>
         </div>
 
-        <div className={style.msg_row}>
-          <div className={style.msg_text}>
-            {/* <div className={style.u_icon}> */}
 
-            <img className={style.background_icon} alt="bg_img" src={uicon} />
-            <div>
-              <p className={style.para_text}>
-                <p className={style.uname}>Peter</p>
-                <p className={style.pra}>
-                  {" "}
-                  Hello Guys! Whats your opinion?
-                  <span style={{ fontSize: "6px" }}>
-                    &nbsp;&nbsp;&nbsp;&nbsp;12:02 pm
-                  </span>
-                </p>
-              </p>
+        <div className={style.chat_content}>
+          <div className={style.upercontent}>
+            <div className={style.msg_row}>
+              <div className={style.msg_text}>
+                {/* <div className={style.u_icon}> */}
+
+                <img className={style.background_icon} alt="bg_img" src={uicon} />
+                <div>
+                  <p className={style.para_text}>
+                    <p className={style.uname}>Peter</p>
+                    <p className={style.pra}>
+                      {" "}
+                      Hello Guys! Whats your opinion?
+                      <span style={{ fontSize: "6px" }}>
+                        &nbsp;&nbsp;&nbsp;&nbsp;12:02 pm
+                      </span>
+                    </p>
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        <div className={style.msg_row}>
-          <div className={style.msg_text}>
-            {/* <div className={style.u_icon}> */}
+            <div className={style.msg_row}>
+              <div className={style.msg_text}>
+                {/* <div className={style.u_icon}> */}
 
-            <img className={style.background_icon} alt="bg_img" src={uicon} />
-            <div>
-              <p className={style.para_text}>
-                <p className={style.uname}>Peter</p>
-                <p className={style.pra}>
-                  {" "}
-                  Images are better.
-                  <span style={{ fontSize: "6px" }}>
-                    &nbsp;&nbsp;&nbsp;&nbsp;12:03 pm
-                  </span>
-                </p>
-              </p>
+                <img className={style.background_icon} alt="bg_img" src={uicon} />
+                <div>
+                  <p className={style.para_text}>
+                    <p className={style.uname}>Peter</p>
+                    <p className={style.pra}>
+                      {" "}
+                      Images are better.
+                      <span style={{ fontSize: "6px" }}>
+                        &nbsp;&nbsp;&nbsp;&nbsp;12:03 pm
+                      </span>
+                    </p>
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        {/* <div className={style.msg_row}> */}
-        <div className={`${style.msg_row} ${style.msg_row2}`}>
-          <div className={style.msg_text}>
-            {/* <div className={style.u_icon}> */}
 
-            {/* <img
+
+
+
+
+
+
+
+
+            {/* <div className={style.msg_row}> */}
+            <div className={`${style.msg_row} ${style.msg_row2}`}>
+              <div className={style.msg_text}>
+                {/* <div className={style.u_icon}> */}
+
+                {/* <img
               className={style.background_icon}
               alt="bg_img"
               src={uicon}
             /> */}
-            <div>
-              <p className={style.para_text}>
-                {/* <p className={style.uname}>Peter</p> */}
-                <p className={style.yourname}>You</p>
-                <p className={style.your_text}>
-                  {" "}
-                  Yes, it will decrease the loading{" "}
-                  <span style={{ fontSize: "6px" }}>
-                    &nbsp;&nbsp;&nbsp;&nbsp;12:00 pm
-                  </span>
-                </p>
-              </p>
+                <div>
+                  <p className={style.para_text}>
+                    {/* <p className={style.uname}>Peter</p> */}
+                    <p className={style.yourname}>You</p>
+                    <p className={style.your_text}>
+                      {" "}
+                      Yes, it will decrease the loading{" "}
+                      <span style={{ fontSize: "6px" }}>
+                        &nbsp;&nbsp;&nbsp;&nbsp;12:00 pm
+                      </span>
+                    </p>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={style.msg_row}>
+              <div className={style.msg_text}>
+                {/* <div className={style.u_icon}> */}
+
+                <img className={style.background_icon} alt="bg_img" src={uicon} />
+                <div>
+                  <p className={style.para_text}>
+                    <p className={style.uname}>Peter</p>
+                    <p className={style.pra}>
+                      {" "}
+                      Anyone is up for illustration. I think there are less
+                      relatable images according to our brand
+                      <span style={{ fontSize: "6px" }} className={style.tiime}>
+                        12:05 pm
+                      </span>
+                    </p>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={style.msg_row}>
-          <div className={style.msg_text}>
-            {/* <div className={style.u_icon}> */}
-
-            <img className={style.background_icon} alt="bg_img" src={uicon} />
-            <div>
-              <p className={style.para_text}>
-                <p className={style.uname}>Peter</p>
-                <p className={style.pra}>
-                  {" "}
-                  Anyone is up for illustration. I think there are less
-                  relatable images according to our brand
-                  <span style={{ fontSize: "6px" }} className={style.tiime}>
-                    12:05 pm
-                  </span>
-                </p>
-              </p>
-            </div>
-          </div>
-        </div>
-
-        {/* <div className={style.space_bar}>
+          {/* <div className={style.space_bar}>
           space
         </div> */}
 
-        <div className={style.typing}>
-          <img className={style.background_icon} alt="bg_img" src={ticon} />
-          <p className={style.p_typing}>peter is typing</p>
-        </div>
-        <div className={style.message_bar}>
-          <button className={style.share_btn}>
-            <img className={style.background_i} alt="bg_img" src={sicon} />
-          </button>
-          <input type="text" placeholder="Write message here" />
-          <button className={style.send_btn}>
-            <img className={style.background_ic} alt="bg_img" src={sendicon} />
-          </button>
+          <div className={style.spc}>
+            <div className={style.typing}>
+              <img className={style.background_icon} alt="bg_img" src={ticon} />
+              <p className={style.p_typing}>peter is typing</p>
+            </div>
+            <div className={style.message_bar}>
+              <button className={style.share_btn}>
+                <img className={style.background_i} alt="bg_img" src={sicon} />
+              </button>
+              <input type="text" placeholder="Write message here" />
+              <button className={style.send_btn}>
+                <img className={style.background_ic} alt="bg_img" src={sendicon} />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
