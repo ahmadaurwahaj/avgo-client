f43146ae9711ce1ecf1e56f85122d8cf7225236e
Ahmad1234Mehmood <ahmad.mehmood1236@gmail.com>
2023-06-01 09:53:53 +0500

    message-ui-updated

--- src/Pages/Settings/Settings.jsx
+++ src/Pages/Settings/Settings.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import style from "./Settings.module.css";
+import SideBar from "../../Components/Sidebar/SideBar";
+
 import { Link } from "react-router-dom";
 function Settings() {
   return (
-    <div>
+
+    <div className={style.main}>
+      <div className={style.inner}>
+      <div>
+          <SideBar/>
+        </div>
+        <div className={style.left_container}></div>
+        <div className={style.Rec_1}>
+        <div className={style.Rec_2}>
+      
+
+</div>
+        </div>
+
+      </div>
+    {/* <div>
       <p>Settings</p>
       <Link to="/video-calling">VideoCalling </Link>
       <Link to="/messages">Message </Link>
       <Link to="/settings">Settings </Link>
+    </div> */}
     </div>
   );
 }
