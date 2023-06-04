98597543ce5626fc659331a99fd9ed20d4b65b9d
m-asad-ali <masadali304@gmail.com>
2023-06-04 17:03:37 +0500

    changed location of api files

--- src/utils/api/authApi.js
+++ src/utils/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "./axios";
 
-export const signup = async (user) => {
+export const signup = async user => {
   try {
     const response = await axios.post(`/api/v1/users/signup`, user);
     return response.data;
@@ -9,19 +9,19 @@ export const signup = async (user) => {
   }
 };
 
-export const signup2 = async (user) => {
+export const signup2 = async user => {
   console.log("🚀 ~ file: authApi.js:14 ~ signup2 ~ user:", user);
   try {
     const headers = {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${user?.token}`,
+      Authorization: `Bearer ${user?.token}`
     };
 
     const response = await axios.put(
       `/api/v1/users/${user.id}`,
       { ...user, type: "signup" },
       {
-        headers,
+        headers
       }
     );
 
@@ -31,7 +31,7 @@ export const signup2 = async (user) => {
   }
 };
 
-export const login = async (user) => {
+export const login = async user => {
   try {
     const response = await axios.post("/api/v1/users/login", user);
     return response.data;
