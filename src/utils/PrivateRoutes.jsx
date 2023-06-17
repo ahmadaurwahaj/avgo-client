40564cb154f3b224e12e0313226463ba4506ee47
m-asad-ali <masadali304@gmail.com>
2023-06-17 13:38:56 +0500

    removed some code

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -3,16 +3,9 @@ import { useSelector } from "react-redux";
 
 function PrivateRoute({ children }) {
   const data = "OK";
-  // const isLogedIn = useSelector((state) => state?.user?.isLogedIn);
   const isLoggedIn = useSelector((state) => state?.user?.isLoggedIn);
 
-  return !isLoggedIn ? (
-    <>
-      <Navigate to="/login" />
-    </>
-  ) : (
-    children
-  );
+  return !isLoggedIn ? <Navigate to="/login" /> : children;
 }
 
 export { PrivateRoute };
